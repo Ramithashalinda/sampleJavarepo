e35a2fc1a88e429eecde1ab516ec8228d1950730
ramitha <ramithashalinda99@gmail.com>
2021-07-01 21:30:44 +0530

    Add calculation

---
+++ src/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+    static void  show()
+    {
+        int x=5,y=10,t;
+        t=0;
+        t=y+x;
+
+        System.out.println("answer is "+ t);
+    }
+
+    public static void main(String[] args) {
+
+
+    show();
+    }
+}
